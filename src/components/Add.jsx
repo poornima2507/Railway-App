3ef7accb03b6b73f2a137381d6b84178cca66ba7
poornima2507 <poornimaofficial25@gmail.com>
2024-06-12 21:52:01 +0530

    created

---
+++ src/components/Add.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Add = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Name</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Age</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Source</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Destination</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Date</label>
+                                <input type="date" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Phone no</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-success">Book Ticket</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Add
